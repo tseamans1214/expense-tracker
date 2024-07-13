763f64c6e3c8cbcec6c80db6e6b30fb2016640e2
Tommy Seamans <tlseamans1214@gmail.com>
2024-07-12 18:33:43 -0700

    Add initial bars and asssets for nav

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+    {
+      imgURL: "/assets/home.png",
+      route: "/",
+      label: "Home",
+    },
+    {
+      imgURL: "/assets/calendar.png",
+      route: "/recurring-payments",
+      label: "Recurring Payments",
+    },
+    {
+      imgURL: "/assets/circle-1.png",
+      route: "/one-time-payments",
+      label: "One Time Payments",
+    },
+    {
+      imgURL: "/assets/dollar",
+      route: "/income",
+      label: "Income",
+    }
+  ];
