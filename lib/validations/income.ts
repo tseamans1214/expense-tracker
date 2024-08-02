2f58be5239e9f84a7391c534338d27a27549cb26
Tommy Seamans <tlseamans1214@gmail.com>
2024-08-01 21:17:21 -0700

    Fix bug with validations because it had a username which caused it not to register when the submit button was clicked since income has no username

--- lib/validations/income.ts
+++ lib/validations/income.ts
@@ -1,8 +1,8 @@
 import * as z from 'zod';
 
 export const IncomeValidation = z.object({
-    amount: z.number().positive(),
+    userId: z.string(),
+    amount: z.any(),
     source: z.string().min(1, {message: 'Minimum 1 character'}).max(30),
-    username: z.string().min(1, {message: 'Minimum 1 character'}).max(30),
-    description: z.string()
+    description: z.string(),
 })
