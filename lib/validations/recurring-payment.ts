c950412439206c5c048535254f61e53ec1b73369
Tommy Seamans <tlseamans1214@gmail.com>
2024-08-09 18:16:22 -0700

    remove old validations

--- lib/validations/recurring-payment.ts
+++
@@ -1,7 +0,0 @@
-import * as z from 'zod';
-
-export const RecurringPaymentValidation = z.object({
-    userId: z.string(),
-    amount: z.any(),
-    source: z.string().min(1, {message: 'Minimum 1 character'}).max(15),
-})
